97123fb8dd98e2d115cc1db9dc3478d8113236b2
Mikhail Lebedinets <mhlebd@gmail.com>
2022-07-13 21:34:26 +0300

    Add swipes

--- main.py
+++ main.py
@@ -33,6 +33,57 @@ def resize(i):
     listOfGlobals = globals()
     listOfGlobals['resize_coof'] = i
 
+def swipe(i):
+    _screen = Image.open(_rundir + "/screen.png")
+    _width, _height = _screen.size
+
+    _new_width = (_width // resize_coof) * resize_coof
+    _new_height = (_height // resize_coof) * resize_coof
+
+    _center_y = _new_height // 2
+    _center_x = _new_width // 2
+
+    _width4 = _new_width // 4
+    _height3 = _new_height // 3
+
+    if i == 1:
+        _x1 = 0 + _width4
+        _x2 = _new_width - _width4
+        _y1 = _center_y
+        _y2 = _center_y
+    
+    if i == 2:
+        _x1 = _new_width - _width4
+        _x2 = _width4
+        _y1 = _center_y
+        _y2 = _center_y
+    
+    if i == 3:
+        _x1 = _center_x
+        _x2 = _center_x
+        _y1 = _new_height - _height3
+        _y2 = _height3
+    
+    if i == 4:
+        _x1 = _center_x
+        _x2 = _center_x
+        _y1 = _height3
+        _y2 = _new_height - _height3
+
+    if i == 5:
+        _x1 = 10
+        _x2 = 10
+        _y1 = 0
+        _y2 = _center_y
+    
+    if i == 6:
+        _x1 = _new_width - 10
+        _x2 = _new_width - 10
+        _y1 = 0
+        _y2 = _center_y
+    
+    os.system(_adb_path + " shell input swipe " + str(_x1) + " " + str(_y1) + " " + str(_x2) + " " + str(_y2) + " 500")
+
 def screenUpdater():
     while True:
         os.system(_adb_path + " exec-out screencap -p > " + _rundir + "/screen.png")
@@ -50,6 +101,11 @@ def screenUpdater():
         label1.place(x = 0, y = 0)
 
 def main():
+    os.system(_adb_path + " kill-server")
+    os.system(_adb_path + " devices")
+
+    input("Check your device then press Enter...")
+
     updaterThread = threading.Thread(target=screenUpdater)
     updaterThread.start()
 
@@ -60,6 +116,15 @@ def main():
     buttonmenu.add_command(label = "Menu", command= lambda: sendButton("1"))
     buttonmenu.add_command(label = "Recent", command= lambda: sendButton("187"))
     menubar.add_cascade(label="Button", menu=buttonmenu)
+
+    swipemenu = Menu(menubar, tearoff=0)
+    swipemenu.add_command(label = "Center Left -> Center Right", command= lambda: swipe(1))
+    swipemenu.add_command(label = "Center Right -> Center Left", command= lambda: swipe(2))
+    swipemenu.add_command(label = "Center Down -> Center Up", command= lambda: swipe(3))
+    swipemenu.add_command(label = "Center Up -> Center Down", command= lambda: swipe(4))
+    swipemenu.add_command(label = "Left Up -> Left Center (Notifications)", command= lambda: swipe(5))
+    swipemenu.add_command(label = "Right Up -> Right Center (Notifications)", command= lambda: swipe(6))
+    menubar.add_cascade(label="Swipe", menu=swipemenu)
 
     resizemenu = Menu(menubar, tearoff=0)
     resizemenu.add_command(label = "1x", command= lambda: resize(1))
