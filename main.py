e5d0fe6e0382e698c9dff4f6b7141032a90a05af
Mikhail Lebedinets <mhlebd@gmail.com>
2022-07-13 16:16:42 +0300

    Some new features

--- main.py
+++ main.py
@@ -19,14 +19,19 @@ elif _platform == "Darwin":
 root = Tk()
 root.title("pytksc")
 
-# EDIT HERE
-RESIZE_COOF = 4
-# EDIT HERE
+resize_coof = 2
 
 def click(event):
-    x = event.x * RESIZE_COOF
-    y = event.y * RESIZE_COOF
+    x = event.x * resize_coof
+    y = event.y * resize_coof
     os.system(_adb_path + " shell input tap " + str(x) + " " + str(y))
+
+def sendButton(key):
+    os.system(_adb_path + " shell input keyevent " + key)
+
+def resize(i):
+    listOfGlobals = globals()
+    listOfGlobals['resize_coof'] = i
 
 def screenUpdater():
     while True:
@@ -34,8 +39,8 @@ def screenUpdater():
         _screen = Image.open(_rundir + "/screen.png")
         _width, _height = _screen.size
 
-        _new_width = _width // RESIZE_COOF
-        _new_height = _height // RESIZE_COOF
+        _new_width = _width // resize_coof
+        _new_height = _height // resize_coof
         _screen = _screen.resize((_new_width, _new_height), Image.Resampling.LANCZOS)
         root.geometry(str(_new_width) + "x" + str(_new_height))
 
@@ -47,6 +52,23 @@ def screenUpdater():
 def main():
     updaterThread = threading.Thread(target=screenUpdater)
     updaterThread.start()
+
+    menubar = Menu(root)
+    buttonmenu = Menu(menubar, tearoff=0)
+    buttonmenu.add_command(label = "Back", command= lambda: sendButton("4"))
+    buttonmenu.add_command(label = "Home", command= lambda: sendButton("3"))
+    buttonmenu.add_command(label = "Menu", command= lambda: sendButton("1"))
+    buttonmenu.add_command(label = "Recent", command= lambda: sendButton("187"))
+    menubar.add_cascade(label="Button", menu=buttonmenu)
+
+    resizemenu = Menu(menubar, tearoff=0)
+    resizemenu.add_command(label = "1x", command= lambda: resize(1))
+    resizemenu.add_command(label = "2x", command= lambda: resize(2))
+    resizemenu.add_command(label = "3x", command= lambda: resize(3))
+    resizemenu.add_command(label = "4x", command= lambda: resize(4))
+    menubar.add_cascade(label="Resize", menu=resizemenu)
+
+    root.config(menu=menubar)
     root.bind("<Button-1>", click)
     root.mainloop()
 
